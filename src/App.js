e33c595070209d34384c81e1d40f9098e5a34ed1
erdisaruhan <erdi.saruhan@gmail.com>
2021-03-30 20:44:58 +0300

    Complete functionality of project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, { useState } from 'react'
+import './index.css'
+
 function App() {
+
+const [quote, setQuote] = useState( {
+  textQuote:"",
+  authorQuote:""
+})
+
+async function generateQuote() {
+  
+  try {
+    const response = await fetch("https://api.quotable.io/random")
+    var quoteData = await response.json();
+
+    setQuote({textQuote:quoteData.content,authorQuote:quoteData.author})
+  } catch(err){
+    console.error(err);
+}
+
+}
+window.onload = generateQuote;
+
+
   return (
-    <div className="App">
-    
+    <div id="quote-box">
+      <p id="text">{quote.textQuote}</p>
+      <p id="author">{quote.authorQuote}</p>
+      <button id="tweet-quote">Twitter</button>
+      <button id="new-quote" onClick={generateQuote}>New Quote</button>
     </div>
   );
 }
